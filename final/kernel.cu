ae50a0449d315d39d83383c71ea55d08dae708d8
Ubuntu <ubuntu@ip-172-31-38-139.us-east-2.compute.internal>
2019-05-10 05:48:51 +0000

    back up

--- final/kernel.cu
+++ final/kernel.cu
@@ -2,6 +2,8 @@
 #define _KMEANS_KERNEL_H_
 
 #include <stdio.h>
+#define BLOCKSIZE_16 16
+#define DIMENSION 8
 /*
   if the data is consequent, use the add reduction, here I use atomic add.
  */
@@ -41,13 +43,51 @@ __device__ float distance(float *dev_obj_data, float *dev_center_data, int dimen
 /*
   without shared memory
  */
-__global__ void cal_distance(float *dev_obj_data,float *dev_center_data, float *dev_disntance, int dimension, int obj_num){
+__global__ void cal_distance(float *dev_obj_data,float *dev_center_data, float *dev_disntance, int obj_num, int cluster_num, int dimension){
     int col = blockDim.x * blockIdx.x + threadIdx.x;
     int row = blockDim.y * blockIdx.y + threadIdx.y;
-    if(row < obj_num && col < dimension){
-        int idx = col + row * dimension;
-        /* dev_disntance[idx] = distance(dev_obj_data); */
+    if(row < obj_num && col < cluster_num){
+        int idx = col + row * cluster_num;
+        dev_disntance[idx] = distance(dev_obj_data + row * dimension, dev_center_data + col * dimension, dimension);
     }
 }
 
+__global__ void cal_distance_share(float *dev_obj_data, float *dev_center_data, float *dev_distance, int obj_num, int cluster_num, int dimension){
+    int col = blockDim.x * blockIdx.x + threadIdx.x;
+    int row = blockDim.y * blockIdx.y + threadIdx.y;
+    /* const int size = dimension * BLOCKSIZE_16; */
+    const int size = DIMENSION * BLOCKSIZE_16;
+    __shared__ float cluster_center_share[BLOCKSIZE_16][size];
+    __shared__ float obj_data_share[BLOCKSIZE_16][size];
+    /* extern __shared__ float cluster_center_share[]; */
+    /* extern __shared__ float obj_data_share[]; */
+    if(row < obj_num){
+        float *obj_data = &dev_obj_data[dimension * blockDim.y * blockIdx.y];
+        float *center_data = &dev_center_data[dimension * blockDim.x * blockIdx.x];
+        for (int xidx = threadIdx.x; xidx < dimension; xidx += BLOCKSIZE_16) {
+            int idx = dimension * threadIdx.y + xidx;
+            obj_data_share[threadIdx.y][xidx] = obj_data[idx];
+            cluster_center_share[threadIdx.y][xidx] = center_data[idx];
+        }
+        __syncthreads();
+    }
+    if(col < cluster_num && row < obj_num){
+        dev_distance[row * cluster_num + col] = distance(obj_data_share[threadIdx.y], cluster_center_share[threadIdx.x], dimension);
+    }
+}
+__global__ void update_cluster_index(float *dev_distance, int *dev_cluster_info, int obj_num, int cluster_num){
+    int index = threadIdx.x + blockIdx.x * blockDim.x;
+    if(index < obj_num){
+        float *p = &dev_distance[index * cluster_num];
+        int tmp_idx = 0;
+        float tmp_distance = p[0];
+        for (int i = 1; i < cluster_num; ++i) {
+            if(tmp_distance >= p[i]){
+                tmp_distance = p[i];
+                tmp_idx = i;
+            }
+        }
+        dev_cluster_info[index] = tmp_idx;
+    }
+}
 #endif
--- final/kmeans.cu
+++ final/kmeans.cu
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <float.h>
+#include <time.h>
 #include "kernel.cu"
 
-#define DEFAULT_CLUSTER_NUM 2
+#define DEFAULT_CLUSTER_NUM 3
 #define BLOCKSIZE_16 16
 #define BLOCKSIZE_32 32
 #define MAX_ITERATION 10000
@@ -110,6 +111,7 @@ void update_cluter_info(params params, float *cluster_centers, int *cluster_info
     float distance;
     float tmp;
     int idx;
+    float *host_distance = (float*)malloc(sizeof(float) * k * params.obj_num);
     for (int i = 0 ; i < params.obj_num; ++i) {
         idx = 0;
         distance = FLT_MAX;
@@ -119,13 +121,22 @@ void update_cluter_info(params params, float *cluster_centers, int *cluster_info
                 tmp += (params.data[i * dimension + m] - cluster_centers[j * dimension + m]) *
                     (params.data[i * dimension + m] - cluster_centers[j * dimension + m]);
             }
-            if(tmp < distance){
+            host_distance[i * k + j] = tmp;
+            if(tmp <= distance){ //have to use equal 
                 idx = j;
                 distance = tmp;
             }
         }
         cluster_info[i] = idx;
     }
+    /* printf("Host_distance:\n"); */
+    /* for (int i = 0; i < params.obj_num; ++i) { */
+    /*     for (int j = 0; j < k; ++j) { */
+    /*         printf("%f ", host_distance[i * k + j]); */
+    /*     } */
+    /*     printf("\n"); */
+    /* } */
+
 }
 int update_cluster_center(params params, float *cluster_centers_old, int *cluster_info){
     int flag = 0;
@@ -159,7 +170,7 @@ int update_cluster_center(params params, float *cluster_centers_old, int *cluste
             cluster_centers_old[i] = new_center[i];
         }
     }
-    free(new_center);
+    /* free(new_center); */
     return flag;
 }
 /*
@@ -171,9 +182,20 @@ int update_cluster_center(params params, float *cluster_centers_old, int *cluste
 
 */
 void kmeans_gpu(params params, int *cluster_info){
+    /* cudaEvent_t mcp_start, mcp_end; */
+    /* float mcp_time; */
+    /* cudaEventCreate(&mcp_start); */
+    /* cudaEventCreate(&mcp_end); */
+    /* cudaEventRecord(mcp_start,0); */
+
+    cudaEvent_t start, stop;
+    float time;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start, 0);
     float *dev_data, *dev_center_data;
     cudaMalloc((void**)&dev_data, params.obj_num * params.obj_dimension * sizeof(float));
-    cudaMalloc((void**)&dev_center_data, params.cluster_num * params.obj_num * sizeof(float));
+    cudaMalloc((void**)&dev_center_data, params.cluster_num * params.obj_dimension * sizeof(float));
     cudaMemcpy(dev_data, params.data, params.obj_num * params.obj_dimension * sizeof(float), cudaMemcpyHostToDevice);
     int *dev_cluster_info;
     cudaMalloc((void**)&dev_cluster_info, params.obj_num * sizeof(int));
@@ -182,6 +204,11 @@ void kmeans_gpu(params params, int *cluster_info){
     cudaMalloc((void**)&dev_distance, params.obj_num * params.cluster_num * sizeof(float));
     int *dev_cluster_count;
     cudaMalloc((void**)&dev_cluster_count, params.cluster_num * sizeof(int));
+
+    /* cudaEventRecord(mcp_end,0); */
+    /* cudaEventSynchronize(mcp_end); */
+    /* cudaEventElapsedTime(&mcp_time, mcp_start, mcp_end); */
+    /* fprintf(stderr, "gpu memory copy time: %f ms\n", mcp_time); */
     /*
       int *host_cluster_info = (int*)malloc(sizeof(int) * params.obj_num);
       cudaMemcpy(host_cluster_info, dev_cluster_info, sizeof(int)* params.obj_num, cudaMemcpyDeviceToHost);
@@ -205,45 +232,76 @@ void kmeans_gpu(params params, int *cluster_info){
     dim3 grid2d_cluster((params.obj_dimension + BLOCKSIZE_16 - 1) / BLOCKSIZE_16, (params.cluster_num + BLOCKSIZE_16 -1) / BLOCKSIZE_16);
     dim3 grid2d_dimension_obj((params.obj_dimension + BLOCKSIZE_16 - 1) / BLOCKSIZE_16, (params.obj_num + BLOCKSIZE_16 - 1) / BLOCKSIZE_16);
     dim3 grid2d_cluster_obj((params.cluster_num + BLOCKSIZE_16 - 1) / BLOCKSIZE_16, (params.obj_num + BLOCKSIZE_16 - 1) / BLOCKSIZE_16);
-    dim3 grid2d_objNum_16(1, (params.obj_num + BLOCKSIZE_16 - 1) / BLOCKSIZE_16);
-
-    cudaEvent_t start, stop;
-    float time;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
-    cudaEventRecord(start, 0);
 
 
+
+    for(int k = 0; k < params.max_iterator; k++){
     cudaMemset(dev_center_data, 0, params.obj_dimension * params.cluster_num * sizeof(float));
     cudaMemset(dev_cluster_count, 0, params.cluster_num * sizeof(int));
+
+    cudaMemset(dev_distance, 0, params.obj_num * params.cluster_num * sizeof(float));
+
     cal_center_total<<<grid2d_dimension_obj, block2d_16>>>(dev_data, dev_cluster_info, dev_center_data, params.obj_dimension, params.obj_num);
-    /* count_cluster_num<<<grid1d_16, block1d_16>>>(dev_cluster_info, dev_cluster_count, params.obj_dimension, params.obj_num); */
-    count_cluster_num<<<grid1d_32, block1d_32>>>(dev_cluster_info, dev_cluster_count, params.obj_dimension, params.obj_num);
+    count_cluster_num<<<grid1d_16, block1d_16>>>(dev_cluster_info, dev_cluster_count, params.obj_dimension, params.obj_num);
+    /* count_cluster_num<<<grid1d_32, block1d_32>>>(dev_cluster_info, dev_cluster_count, params.obj_dimension, params.obj_num); */
     cal_center_mean<<<grid2d_cluster, block2d_16>>>(dev_center_data, dev_cluster_count, params.obj_dimension, params.cluster_num);
-    /* cal_distance<<<grid2d_cluster_obj, block2d_16>>>(dev_data, dev_center_data, dev_distance, params.obj_dimension, params.obj_num); */
+    cal_distance<<<grid2d_cluster_obj, block2d_16>>>(dev_data, dev_center_data, dev_distance, params.obj_num, params.cluster_num, params.obj_dimension);
+    /* cal_distance_share<<<grid2d_cluster_obj, block2d_16, params.obj_dimension * BLOCKSIZE_16 * sizeof(float) * 2>>>(dev_data, dev_center_data, dev_distance, params.obj_num, params.cluster_num, params.obj_dimension); */
+    update_cluster_index<<<grid1d_16,block1d_16>>>(dev_distance, dev_cluster_info, params.obj_num, params.cluster_num);
+    }
 
-
-    int *host_cluster_info;
     float *host_center_data;
     host_center_data = (float*)malloc(params.obj_num * params.obj_dimension * sizeof(float));
-    host_cluster_info = (int*)malloc(params.cluster_num * sizeof(int));
     cudaMemcpy(host_center_data, dev_center_data, params.obj_dimension * params.cluster_num * sizeof(float), cudaMemcpyDeviceToHost);
-    /* for (int i = 0; i < params.max_iterator; ++i) { */
+    /* float *host_distance = (float*)malloc(params.cluster_num * params.obj_num * sizeof(float)); */
+    /* cudaMemcpy(host_distance, dev_distance, params.cluster_num * params.obj_num * sizeof(float), cudaMemcpyDeviceToHost); */
+    /* int *host_count = (int*)malloc(params.cluster_num * sizeof(int)); */
+    /* cudaMemcpy(host_count, dev_cluster_count, params.cluster_num * sizeof(int), cudaMemcpyDeviceToHost); */
 
+    /* for (int i = 0; i < params.cluster_num; ++i) { */
+    /*     printf("cluster%d count: %d\n", i, host_count[i]); */
     /* } */
 
+    /* for (int i = 0; i < params.obj_num; ++i) { */
+    /*     for (int j = 0; j < params.cluster_num; ++j) { */
+    /*         printf("%f ", host_distance[i * params.cluster_num + j]); */
+    /*     } */
+    /*     printf("\n"); */
+    /* } */
+
+    cudaMemcpy(cluster_info, dev_cluster_info, params.obj_num * sizeof(int), cudaMemcpyDeviceToHost);
     cudaEventRecord(stop, 0);
     cudaEventSynchronize(stop);
     cudaEventElapsedTime(&time, start, stop);
-    fprintf(stderr, "gpu calculate time: %f \n", time);
+    fprintf(stderr, "gpu calculate time: %f ms\n", time);
+
+
+    /*
     for (int i = 0; i < params.cluster_num; ++i) {
         for(int j = 0; j < params.obj_dimension; ++j){
-            fprintf(stderr, "%f ", host_center_data[i * params.obj_dimension + j]);
+            fprintf(stderr, "gpu center: %f ", host_center_data[i * params.obj_dimension + j]);
         }
         fprintf(stderr, "\n");
     }
+    */
+
+
+    /* for (int i = 0 ; i < params.obj_num; ++i) { */
+    /*     printf("obj%d: %d\n", i, cluster_info[i]); */
+    /* } */
+
+    cudaFree(dev_data);
+    cudaFree(dev_cluster_info);
+    cudaFree(dev_distance);
+    cudaFree(dev_center_data);
+    cudaFree(dev_cluster_count);
 }
 void kmeans(params params){
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock();
+
     float *cluster_centers = NULL;
     if((cluster_centers = init_cluster_center(params)) == NULL)
         exit(EXIT_FAILURE);
@@ -255,15 +313,33 @@ void kmeans(params params){
         exit(EXIT_FAILURE);
     }
     int n = 0;
-    /* for (int i = 0; i < params.max_iterator; ++i) { */
-    for (int i = 0; i < 1; ++i) {
-        n++;
+    int *host_cluster_info = (int*)malloc(sizeof(int) * params.obj_num);
+    if(!host_cluster_info){
+        fprintf(stderr, "malloc host_cluster_info error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    update_cluter_info(params, cluster_centers, cluster_info);
+    for (int i = 0; i < params.obj_num; ++i)
+        host_cluster_info[i] = cluster_info[i];
+    update_cluster_center(params, cluster_centers, cluster_info);
+
+    for (int i = 0; i < params.max_iterator; ++i) {
+    /* for (int i = 1; i < 2; ++i) { */
         update_cluter_info(params, cluster_centers, cluster_info);
+        n++;
         if(update_cluster_center(params, cluster_centers, cluster_info) == 0)
             break;
     }
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
     fprintf(stderr,"iterator %d times\n",n);
+    fprintf(stderr, "cpu_time: %f ms\n", cpu_time_used * 1000);
     params.max_iterator = n;
+
+
+    /*
+
     for (int i = 0; i < params.cluster_num; ++i) {
         fprintf(stderr,"centers%d: ",i);
         for (int j=0; j < params.obj_dimension; ++j) {
@@ -271,13 +347,17 @@ void kmeans(params params){
         }
         printf("\n");
     }
+
+    */
+
     /* for (int i = 0; i < params.obj_num; ++i) { */
     /*     printf("obj%d is in cluster%d\n",i,cluster_info[i]); */
     /* } */
 
-    for (int i = 0; i < params.obj_num; ++i)
-        printf("Index %d : %d\n",i,cluster_info[i]);
-    kmeans_gpu(params, cluster_info);
+    /* for (int i = 0; i < params.obj_num; ++i) */
+    /*     printf("obj%d: %d\n",i,cluster_info[i]); */
+    /* kmeans_gpu(params, host_cluster_info); */
+    kmeans_gpu(params, host_cluster_info);
 }
 int main(int argc, char *argv[]){
     params params;
@@ -286,6 +366,7 @@ int main(int argc, char *argv[]){
         params.cluster_num = atoi(argv[2]);
     else
         params.cluster_num = DEFAULT_CLUSTER_NUM;
+    /* printf("cluster_num: %d\n",params.cluster_num); */
     /* for (int i = 0; i < params.obj_dimension * params.obj_num; ++i) { */
     /*     printf("%f ",params.data[i]); */
     /*     if((i - params.obj_dimension + 1) % (params.obj_dimension) == 0) */
@@ -294,5 +375,4 @@ int main(int argc, char *argv[]){
     kmeans(params);
     free(params.data);
     exit(EXIT_SUCCESS);
-
 }
